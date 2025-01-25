b39196348962210cf7f5ac82ee83f609d4fbb344
Wajahat Ali Khan <assim345678@gmail.com>
2025-01-26 03:33:37 +0500

    updated code

--- src/app/shoplist/[slug]/page.tsx
+++ src/app/shoplist/[slug]/page.tsx
@@ -1,12 +1,11 @@
 
 import IProduct from "@/types/foods";
 import Product from "./Product";
-
 import { client } from "@/sanity/lib/client";
 
 async function Productpage({ params }: { params: { slug: string } }) {
-  const product:IProduct =
-    await client.fetch(`*[_type == "food" && slug.current == $slug][0] {
+  const product: IProduct = await client.fetch(
+    `*[_type == "food" && slug.current == $slug][0] {
       name,
       description,
       price,
@@ -14,18 +13,19 @@ async function Productpage({ params }: { params: { slug: string } }) {
       tags,
       "imageUrl": image.asset->url,
       "slug": slug.current,
-   }`,{slug:params.slug});
-   
+   }`,
+    { slug: params.slug }
+  );
+
   return (
     <>
-        
-        {product ? (
-          <Product product={product} />
-        ) : (
-          <div className="text-center text-2xl my-10 font-bold">
-            <p>Opps ! Product not found.</p>
-          </div>
-        )}
+      {product ? (
+        <Product product={product} />
+      ) : (
+        <div className="text-center text-2xl my-10 font-bold">
+          <p>Oops! Product not found.</p>
+        </div>
+      )}
     </>
   );
 }
